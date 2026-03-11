825f65b0717936b2a1d46c8e9e6277be7199e145
Oppliger J. <julien@oppliger.pro>
2026-03-11 15:15:55 +0100

    add correction exo 10

--- concept_niveau_2.py
+++ concept_niveau_2.py
@@ -1,14 +1,43 @@
-notes = [20, 14, 12]
+## █████████████ 
+## LES FONCTIONS
+## █████████████ 
 
-# Dicitonnaire
+# -- 📕 Définition
+# Une fonction :
+# est un bloc de code réutilisable et paramétrable.
 
-coord = { 
-    "latitude": 0.5678548, 
-    "longitude": 1.48564568 
-}
+# Elle peut prendre des paramètres et retourner une valeur.
+
+# Pour déclarer une fonction :
+#
+# def nom_de_la_fonction(nom_du_parametre: type_du_parametre) -> type_de_retour:
+#     return valeur_de_retour
+
+# eg,
+def somme(notes: list) -> int:
+    return 0
+
+# Pour appeler une fonction est:
+#
+# nom_de_la_fonction(parametre)
+
+total = somme([12, 14, 16])
+
+## █████████████████ 
+## LES DICTIONNAIRES
+## █████████████████ 
+
+# -- 📕 Définition
+# Un dictionnaire (dict) :
+# est un type qui stock un ensemble de valeur sous la forme clé -> valeur.
 
 contact = {
     "prenom": "julien",
     "nom": "oppliger",
     "tel": "0032 2 452 487 45"
 }
+
+# Pour accéder à un élément d'un dictionnaire, on utilise la même syntaxe que
+# pour une liste.
+
+contact["prenom"]
--- exo10.py
+++ exo10.py
@@ -8,14 +8,36 @@ coord_b = {
     "y": 28
  }
 
-def get_highest(coord_a: dict, coord_b: dict) -> dict:
-    # Highest is the greatest Y value
-    return coord_a
 
-def get_farest(coord_a: dict, coord_b: dict) -> dict:
-    # Farest is the greatest X value
-    return coord_a
+# def get_highest(coords: list[dict]) -> dict:
+#     highest = {
+#         "x": 0,
+#         "y": 0,
+#     }
+
+#     for coord in coords:
+#         if coord["y"] > highest["y"]:
+#             highest = coord
+    
+#     return highest
+
+def get_highest(coord_a: dict, coord_b: dict) -> dict:
+    if coord_a["y"] > coord_b["y"]:
+        return coord_a
+
+    return coord_b
+
+
+def get_farthest(coord_a: dict, coord_b: dict) -> dict:
+    if coord_a["x"] > coord_b["x"]:
+        return coord_a
+
+    return coord_b
 
 def get_mid(coord_a: dict, coord_b: dict) -> dict:
-    # Mid is between A and B
-    return coord_a
+    return {
+        "x": (coord_a["x"] + coord_b["x"]) / 2,
+        "y": (coord_a["y"] + coord_b["y"]) / 2
+    }
+
+print(get_mid(coord_a, coord_b))
