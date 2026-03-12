9bae5bb27413eec8d393928e31b6d7e46a14bc61
Oppliger J. <account@oppliger.cc>
2026-03-12 16:05:56 +0100

    add coef.py

--- coef.py
+++ coef.py
@@ -8,3 +8,50 @@
 
 # NB: utilisez str.split(separator) pour séprarer la note du coef
 # https://docs.python.org/3/library/stdtypes.html#str.split
+
+import sys
+
+sys.argv.pop(0)
+
+if len(sys.argv) < 1:
+    print("error: not enough arguments")
+    exit(1)
+
+notes = []
+for arg in sys.argv:
+    splitted = arg.split(":")
+    
+    if len(splitted) != 2:
+        print(f"error: {arg} is note a well formatted note")
+        exit(1)        
+
+    try:
+        note = {
+            "value": float(splitted[0]),
+            "coef": float(splitted[1])
+        }
+    except ValueError:
+        print(f"error: {arg} must be made of numbers")
+        exit(1)
+
+    if note["value"] < 0 or note["value"] > 20:
+        print(f"error: note {note['value']} out of range")
+        exit(1)
+
+    if note["coef"] < 0:
+        print(f"error: coef {note['coef']} out of range")
+        exit(1)
+        
+    notes.append(note)
+        
+
+total = {
+    "value": 0,
+    "coef": 0
+}
+for note in notes:
+    total["value"] = total["value"] + note["value"] * note["coef"]
+    total["coef"] = total["coef"] + note["coef"]
+
+avg = total["value"] / total["coef"]
+print(avg)
